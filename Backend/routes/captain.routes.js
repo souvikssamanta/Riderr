1bc43a3818e09f9d040a3b0e7881b647cea4889c
Souvik Samanta <souvikss2004@gmail.com>
2025-07-06 22:01:19 +0530

    Update captain.routes.js

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -25,7 +25,7 @@ router.post('/login',[
 router.get("/profile",authMiddleware.authCaptain,captainController.getCaptainProfile);
 
 
-router.get('/logout',authMiddleware.authCaptain,captainController.logoutCaptain);
+router.get('/captain-logout',authMiddleware.authCaptain,captainController.logoutCaptain);
 module.exports=router;
 
 
