42851b41bd5593ce7890d81b388ef4c338b88f5a
Souvik Samanta <souvikss2004@gmail.com>
2025-07-07 20:54:26 +0530

    update failure

--- Frontend/src/pages/failure.jsx
+++ Frontend/src/pages/failure.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 const failure = () => {
+  const query = new URLSearchParams(useLocation().search);
+  const paymentId = query.get("reference");
+  
   return (
     <div className="">
       <div className="flex flex-col max-w-2xl justify-center items-center mt-40">
@@ -8,9 +12,14 @@ const failure = () => {
         <p className="text-lg text-gray-700 text-center mb-2">
           Oops! Something went wrong with your payment.
         </p>
+        <p className="text-semibold">
+          Your Payment Id:
+          <span className="text-blue-500 font-bold">{paymentId}</span>
+        </p>
         <p className="text-md text-center text-gray-500 mb-6">
           Please try again or contact support if the issue persists.
         </p>
+
         <button
           className="px-6 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
           onClick={() => (window.location.href = "/")}
