04b4299702d600a15f77deec8b8f8579a9c7553c
Souvik Samanta <souvikss2004@gmail.com>
2025-07-07 21:08:05 +0530

    Create payment.model.js

---
+++ Backend/models/payment.model.js
@@ -0,0 +1,9 @@
+const paymentSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+    min: [0, "Fare must be a positive number"],
+  },
+});
+
+module.exports=mongoose.model('payment',paymentSchema);
